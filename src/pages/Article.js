55ba9daf6912471716a71643e091a8c5c736db22
kewinMarchand <k.marchand@purjus.fr>
2019-10-29 20:21:14 +0100

    Box display

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -4,29 +4,107 @@ import {inject, observer} from 'mobx-react'
 // Theme
 import Theme from "../theme/Theme"
 // UI components
-import {Card, CardContent, CardHeader, Grid, Typography} from '@material-ui/core'
+import {Box, Card, CardContent, Grid, Typography} from '@material-ui/core'
 // custom components
 import PageLayout from '../components/page/PageLayout'
+
+const text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat eum temporibus dicta a beatae dolorem itaque maxime ipsum ipsa numquam, tenetur sequi eligendi odit soluta ratione. In facilis illum perferendis consectetur recusandae rerum libero ullam non nobis, ipsa dolorem soluta facere nulla, reiciendis quidem blanditiis impedit eligendi, atque eveniet. Similique, corporis placeat. Esse fugiat sint vel odio nesciunt nam cum totam placeat, consequuntur expedita consequatur, architecto facere vitae illum. Quasi, optio nobis voluptates cumque animi accusamus quia ex temporibus. Natus exercitationem aliquid molestias, dolorem nemo eaque praesentium commodi voluptas magnam architecto illo facere aperiam, pariatur nulla nobis eligendi aspernatur incidunt?'
 
 class Article extends Component {
 
   render() {
     const { match, stores } = this.props,
-      post = stores.uiStore.posts[match.params.id]
+      { capitalize, posts } = stores.uiStore,
+      post = posts[match.params.id],
+      { body, title } = post
 
     return (
         <PageLayout
             backgroundColor={Theme.palette.primary.main}
             title={'Blog'}
-            subtitle={stores.uiStore.capitalize(post.title)}
+            subtitle={capitalize(title)}
         >
         <Grid container spacing={4}>
-          <Grid item xs={12}>
-            <Card>
+          <Grid item xs={12} align={'center'}>
+            <Card style={{maxWidth: 600}}>
               <CardContent>
-                <Typography>
-                  {stores.uiStore.capitalize(post.body)}
-                </Typography>
+                <Grid container >
+                  <Typography gutterBottom>
+                    {capitalize(body)}
+                  </Typography>
+                </Grid>
+                <Box 
+                  display={{ 
+                    xs: 'none', 
+                    sm: 'block',
+                    md: 'block', 
+                    lg: 'block', 
+                    xl: 'block' 
+                  }}
+                >
+                  <Grid 
+                    container
+                    style={{marginTop: 40}}
+                  >
+                    <Typography gutterBottom>
+                      {text}
+                    </Typography>
+                  </Grid>
+                </Box>
+                <Box 
+                  display={{ 
+                    xs: 'none', 
+                    sm: 'none', 
+                    md: 'block', 
+                    lg: 'block', 
+                    xl: 'block' 
+                  }}
+                >
+                  <Grid 
+                    container
+                    style={{marginTop: 40}}
+                  >
+                    <Typography gutterBottom>
+                      {text}
+                    </Typography>
+                  </Grid>
+                </Box>
+                <Box 
+                  display={{ 
+                    xs: 'none', 
+                    sm: 'none', 
+                    md: 'none', 
+                    lg: 'block',
+                    xl: 'block' 
+                  }}
+                >
+                  <Grid 
+                    container
+                    style={{marginTop: 40}}
+                  >
+                    <Typography gutterBottom>
+                      {text}
+                    </Typography>
+                  </Grid>
+                </Box>
+                <Box 
+                  display={{ 
+                    xs: 'none', 
+                    sm: 'none', 
+                    md: 'none', 
+                    lg: 'none', 
+                    xl: 'block' 
+                  }}
+                >
+                  <Grid 
+                    container
+                    style={{marginTop: 40}}
+                  >
+                    <Typography gutterBottom>
+                      {text}
+                    </Typography>
+                  </Grid>
+                </Box>
               </CardContent>
             </Card>
           </Grid>
